4e290090235b7045f8312393ee417ac2d2521499
Yevgeniy Cherdantsev <yevgeniy.cherdantsev@gmail.com>
2020-03-27 09:27:11 +0600

    Added genres

---
+++ database/migrations/2020_03_27_032558_create_films_genres.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmsGenres extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films_genres', function (Blueprint $table) {
+            $table->integer('film_id');
+            $table->integer('genre_id');
+
+            $table->foreign('film_id')->references('id')->on('films');
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films_genres');
+    }
+}
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/css/style.css" rel="stylesheet">
+    <link href="/foundation-6.6.1/css/app.css" rel="stylesheet">
     @yield('header')
     <title>@yield('title')</title>
 </head>
@@ -14,5 +15,11 @@
 @yield('content')
 </body>
 @include('layouts.footer')
-@yield('footer')
+
+<footer>
+
+    <script src="/foundation-6.6.1/js/app.js" type="application/javascript"></script>
+    @yield('footer')
+
+</footer>
 </html>
